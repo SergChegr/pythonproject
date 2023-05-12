5a4d57bbf49107a927499016a0e28de7c965b884
SergChegr <sergchegr1406@gmail,com>
2023-05-13 01:35:35 +0300

    da

--- cw2.py
+++ cw2.py
@@ -1,29 +1,16 @@
-# class Grandparent:
-#     def about(self):
-#         print("I am GrandParent")
-#         def about_myself(self):
-#             print("I am Grandparent")
-# class Parent(Grandparent):
-#     def about_myself(self):
-#         print("I am Parent")
-# class Child(Parent):
-#     def __init__(self):
-#         super().about()
-#         super().about_myself()
-# nick = Child()
-class Computer:
+class TV:
     def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = model
-        self.memory = 128
+        self.memory = 244
 class Display:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.resolution = "4k"
-class SmartPhone(Display, Computer):
+        self.resolution = "8k"
+class SmartPhone(Display, TV):
     def print_info(self):
         print(self.model)
         print(self.resolution)
         print(self.memory)
-iphone = SmartPhone(model ="Last")
+iphone = SmartPhone(model ="Best")
 iphone.print_info()
