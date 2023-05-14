aaf9d9c6f2b817ed9ea2c2e2097883fcbaa6ba5f
SergChegr <sergchegr1406@gmail,com>
2023-05-14 21:42:45 +0300

    da

--- дз1/hw3.py
+++ дз1/hw3.py
@@ -1,13 +1,13 @@
-# import logging
-# logging.basicConfig(level=logging.DEBUG,
-#                         filename="logs.log", filemode="a",
-#                         format="We have next logging message: "
-#                         "%(asctime)s:%(levelname)s-%(message)s")
-# logging.debug("debug")
-# logging.info("info")
-# logging.warning("warning")
-# logging.error("error")
-# logging.critical("critical")
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                        filename="logs.log", filemode="a",
+                        format="We have next logging message: "
+                       "%(asctime)s:%(levelname)s-%(message)s")
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
 
 def adder(*args, **kwargs):
     result = 0
